c5e739fc67f842dc82151ff79ac4404773d4f209
jamesyeon <jamesyeon@yahoo.com>
2015-11-09 18:15:27 +0900

    Update JavaHelloWorld.java

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -4,5 +4,6 @@ public class JavaHelloWorld
         {
                 System.out.println("Java Hello Wolrd");
                 System.out.println("Hello again");
+                System.out.println("Hello again JAMES !!!");
         }
 }
